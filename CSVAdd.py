1c001dbfe7d70d2504ed9c91d834f1ab0a97fc4d
Jeyanth C <35778904+JeyanthC@users.noreply.github.com>
2018-10-19 13:20:04 +0530

    Added comments

--- CSVAdd.py
+++ CSVAdd.py
@@ -1,7 +1,9 @@
+#Store the names of the files in a folder to a CSV file
+
 import os, csv
 
-with open("k.csv", 'w') as f:
+with open("/**/", 'w') as f:                    #Insert the name of the CSV File that should store the names of the files in the folder
     writer = csv.writer(f)
-    for path, dirs, files in os.walk("E:\Movies\English"):
+    for path, dirs, files in os.walk("/**/"):   #Insert name of the directory within the /**/ and remove the /**/
         for filename in files:
             writer.writerow([filename])
